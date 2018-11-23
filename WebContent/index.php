e38ba7fd5729b87d5a3ff03723033795b3a447cd
Valle <v.kummert@drmnt.de>
2018-11-23 19:16:31 +0100

    Webpage generation

---
+++ WebContent/index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>der_Hausrat[geber]</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=0.5">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Amatic+SC" />
+	<link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Lato" />
+</head>
+<body>
+
+<div class="main">
+	<div class="headline">
+	<div class="icontext">
+		der_Hausrat[geber]
+	</div>
+	</div>
+
+
+
+	<div class="context">
+		<div class="beitragFett">
+			<h1 style="background-color: rgba(255, 189, 10, 0.50)">Notifycation</h1>
+				 <nav>
+            		<ul>
+		                <li>Link 1</li>
+		                <li>Link 2</li>
+		                <li>Link 3</li>
+		                <li>Link 4</li>
+		                <li>Link 5</li>
+		                <li>Link 6</li> 
+		                <li>Link 7</li> 
+		                <li>Link 8</li>
+		                <li>Link 9</li>
+		                <li>Link 10</li>
+		                <li>Link 11</li>
+		                <li>Link 13</li>
+		                <li>Link 13</li>
+            		</ul>
+        		</nav>
+			</p>
+		</div>
+		<div class="beitragFett">
+			<h1 style="background-color: rgba(255, 189, 10, 0.50)">Inventory</h1>
+			<p>
+				<nav>
+            		<ul>
+		                <li>Link 1</li>
+		                <li>Link 2</li>
+		                <li>Link 3</li>
+		                <li>Link 4</li>
+		                <li>Link 5</li>
+		                <li>Link 6</li> 
+		                <li>Link 7</li> 
+		                <li>Link 8</li>
+		                <li>Link 9</li>
+		                <li>Link 10</li>
+		                <li>Link 11</li>
+		                <li>Link 13</li>
+		                <li>Link 13</li>
+            		</ul>
+        		</nav>
+			</p>
+		</div>
+		
+	</div>
+</div>
+</body>
